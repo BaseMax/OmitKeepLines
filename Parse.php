8dc230c1a94cefcac24f664abe20ea079d519c46
Max Base <MaxBaseCode@Gmail.Com>
2019-05-18 22:05:01 +0430

    Update Parse.php

--- Parse.php
+++ Parse.php
@@ -4,7 +4,7 @@
 * @Name : OmitKeepLines/Parse.php
 * @Version : 1.0
 * @Programmer : Max
-* @Date : 2019-05-17
+* @Date : 2019-05-17, 2019-05-18
 * @Released under : https://github.com/BaseMax/OmitKeepLines/blob/master/LICENSE
 * @Repository : https://github.com/BaseMax/OmitKeepLines
 *
